70805230397d9ace363b075582f04cc7a6b3b7bf
rocco.malvaso <malvasorocco3@gmail.com>
2024-09-11 19:08:23 +0200

    Add User Model

---
+++ Shortly.Client/Data/Models/User.cs
@@ -0,0 +1,12 @@
+namespace Shortly_Client.Data.Models;
+
+public class User
+{
+    public User()
+    {
+        Urls = new List<Url>();
+    }
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public List<Url> Urls { get; set; }
+}
