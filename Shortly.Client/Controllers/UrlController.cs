17ea29af3867cca1d33734cd1c3dcb51205ec0cd
rocco.malvaso <malvasorocco3@gmail.com>
2024-09-11 18:53:12 +0200

    Add test data at UrlController.cs
    - Add test data at UrlController.cs simulation data from Database and passing it to Views using Model.

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -8,7 +8,35 @@ public class UrlController : Controller
     // GET
     public IActionResult Index()
     {
-        return View();
+        //Fake DB Data
+        var allUrls = new List<Url>()
+        {
+            new Url()
+            {
+                Id = 1,
+                OriginalLink = "https://google.com",
+                ShortLink = "lil.ly/834h",
+                NrOfClick = 100,
+                UserId = 12
+            },
+            new Url()
+            {
+                Id = 2,
+                OriginalLink = "https://google.com",
+                ShortLink = "lil.ly/34243",
+                NrOfClick = 12,
+                UserId = 13
+            },
+            new Url()
+            {
+                Id = 3,
+                OriginalLink = "https://google.com",
+                ShortLink = "lil.ly/u324h",
+                NrOfClick = 3,
+                UserId = 14
+            }
+        };
+        return View(allUrls);
     }
 
     public IActionResult Create()
